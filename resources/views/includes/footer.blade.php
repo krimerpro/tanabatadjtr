c1ece195d202117c65a21c6e9d5160f990f9b596
varanustoken <varanustoken@gmail.com>
2022-12-26 22:51:33 +0700

    a

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,40 +1,4 @@
-<?php
-  if(Route::current()->getName() != 'marketplace'){
-?>
 
-<footer class="footer static-bottom footer-dark footer-custom-class" data-midnight="white"><div class="container">
-<div class="footer-wrapper">
-<div class="row">
-  <div class="col-md-5 col-lg-5">&nbsp;</div>
-  <div class="col-md-2 col-lg-2" style="text-align:center">
-    <div class="about">
-      <div class="title animated" data-animation="fadeInUpShorter" data-animation-delay="0.2s">
-        <?php
-          if(!$agent->isMobile()){
-        ?>
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width:100%;height:100%">
-        <?php
-      } else{
-        ?>
-            <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width:50%;height:50%">
-        <?php
-      }
-        ?>
-      </div>
-
-    </div>
-  </div>
-
-    <div class="col-md-5 col-lg-5">&nbsp;</div>
-</div>
-<div class="copy-right mx-auto text-center">
-  <span class="copyright" style="color:white">Copyright &copy; 2022, Tanabata - TEAM</span>
-</div>
-</div>
-</div>
-</footer>
-
-<?php } ?>
 
 <script src="{{ asset('/js/vendors.min.js') }}"></script>
 
